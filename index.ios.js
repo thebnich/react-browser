879b8bb09451e6ecd625601dbc0aac84bb8899f9
Brian Nicholson <thebnich@gmail.com>
2016-05-27 16:53:00 -0700

    Add back/forward buttons

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 'use strict';
 
 import React, { Component } from 'react';
@@ -12,40 +6,65 @@ import {
   StyleSheet,
   Text,
   TextInput,
+  TouchableHighlight,
   View,
   WebView,
 } from 'react-native';
 
+const HEADER = '#ddd';
+const BG_COLOR = '#fff';
+const DEFAULT_URL = 'https://www.google.com';
+
 class Browser extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      uri: 'http://www.google.com',
+      url: DEFAULT_URL,
+      outURL: DEFAULT_URL,
+      backButtonEnabled: false,
+      forwardButtonEnabled: false,
+      loading: true,
     };
   }
 
   inputText: ''
+  textRef: null
+  webViewRef: null
 
   render() {
-    this.inputText = this.state.uri;
-
     return (
       <View style={styles.container}>
-        <View style={styles.urlbar}>
+        <View style={styles.urlBar}>
+          <TouchableHighlight
+            onPress={this.goBack}
+            style={this.state.backButtonEnabled ? styles.navButton : styles.disabledButton}
+            underlayColor='#aaa'
+          >
+            <Text>{'<'}</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={this.goForward}
+            style={this.state.forwardButtonEnabled ? styles.navButton : styles.disabledButton}
+            underlayColor='#aaa'
+          >
+            <Text>{'>'}</Text>
+          </TouchableHighlight>
           <TextInput
-            style={styles.urlbarinput}
-            autoCapitalize='none'
-            autoCorrect={false}
-            keyboardType='url'
-            onChangeText={this.onChangeText}
+            ref={(ref) => this.textRef = ref}
+            style={styles.urlBarInput}
+            autoCapitalize="none"
+            defaultValue={this.state.url}
             onSubmitEditing={this.onSubmitEditing}
+            onChangeText={this.onChangeText}
+            clearButtonMode="while-editing"
           />
         </View>
         <WebView
-          source={{
-            uri: this.state.uri,
-            method: 'GET',
-          }}
+          ref={(ref) => this.webViewRef = ref}
+          source={{uri: this.state.outURL}}
+          onNavigationStateChange={this.onNavigationStateChange}
+          startInLoadingState={true}
         />
       </View>
     );
@@ -55,27 +74,79 @@ class Browser extends Component {
     this.inputText = text;
   }
 
+  goBack = () => {
+    this.webViewRef.goBack();
+  }
+
+  goForward = () => {
+    this.webViewRef.goForward();
+  }
+
+  reload = () => {
+    this.webViewRef.reload();
+  }
+
+  onNavigationStateChange = (navState) => {
+    this.setState({
+      backButtonEnabled: navState.canGoBack,
+      forwardButtonEnabled: navState.canGoForward,
+      url: navState.url,
+      loading: navState.loading,
+    });
+  }
+
   onSubmitEditing = () => {
-    this.setState({ uri: this.inputText });
+    if (!/^[a-zA-Z-_]+:/.test(this.inputText)) {
+      this.inputText = 'http://' + this.inputText;
+    }
+    this.setState({ outURL: this.inputText });
+    this.textRef.blur();
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 20,
     flex: 1,
-    backgroundColor: '#eee',
+    backgroundColor: HEADER,
+    paddingTop: 20,
   },
-  urlbar: {
-    padding: 10,
+  urlBar: {
+    flexDirection: 'row',
+    padding: 8,
   },
-  urlbarinput: {
-    height: 30,
-    backgroundColor: 'white',
-    fontSize: 12,
-    borderWidth: 1,
+  urlBarInput: {
+    backgroundColor: BG_COLOR,
     borderColor: '#aaa',
-    padding: 5,
+    borderWidth: 0.5,
+    height: 28,
+    paddingLeft: 10,
+    paddingTop: 3,
+    paddingBottom: 3,
+    flex: 1,
+    fontSize: 12,
+  },
+  navButton: {
+    width: 28,
+    height: 28,
+    padding: 3,
+    marginRight: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: BG_COLOR,
+    borderColor: '#aaa',
+    borderWidth: 0.5,
+    opacity: 1,
+  },
+  disabledButton: {
+    width: 28,
+    height: 28,
+    padding: 3,
+    marginRight: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    opacity: 0.2,
+    borderColor: '#aaa',
+    borderWidth: 0.5,
   },
 });
 
